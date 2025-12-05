f4f3f92c9d6392da5f4cbe822eaedd62912aa94a
FaizulHassan-Rhine <faizulhassan.rien@gmail.com>
2025-12-05 14:20:15 +0600

    update folder

--- convert_ts_to_js.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Simple script to help track conversion progress
-find . -name "*.tsx" -o -name "*.ts" | grep -v node_modules | grep -v ".next" | grep -v "next-env.d.ts" | while read file; do
-  if [ -f "$file" ]; then
-    echo "Still needs conversion: $file"
-  fi
-done
